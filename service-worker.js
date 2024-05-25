bffbdb71aa7bb45ca3e2ca08030f7ab68e923697
Dest <101679009+destpoland@users.noreply.github.com>
2024-05-25 12:57:39 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,22 @@
 self.addEventListener('install', function () {
   console.log('Service worker installing...');
 });
+
+const cacheName='cache-2024.05.26-01';
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(async function() {
+
+    const cache = await caches.open(cacheName);
+    const cachedResponse = await cache.match(event.request);
+    const networkResponsePromise = fetch(event.request);
+
+    event.waitUntil(async function() {
+      const networkResponse = await networkResponsePromise;
+      await cache.put(event.request, networkResponse.clone());
+    }());
+
+    // Returned the cached response if we have one, otherwise return the network response.
+    return cachedResponse || networkResponsePromise;
+  }());
+});
