146b0c622f9ae866419970cff162902c5d683d71
Destpoland <tyszkakevin@gmail.com>
2024-10-30 16:47:41 -0500

    test

---
+++ search/uv/js/index.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const form = document.getElementById("uv-form");
+const address = document.getElementById("uv-address");
+const uvsearchEngine = document.getElementById("uv-search-engine");
+const error = document.getElementById("uv-error");
+const errorCode = document.getElementById("uv-error-code");
+
+const url = new URLSearchParams(window.location.search);
+const q = url.get('q');
+if (q) {
+  const url = search(q, uvsearchEngine.value);
+  localStorage.setItem('url', __uv$config.prefix + __uv$config.encodeUrl(url));
+  location.href = "/load.html"
+} else {
+}
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  try {
+    await registerSW();
+  } catch (err) {
+    throw err;
+  }
+
+  const url = search(address.value, uvsearchEngine.value);
+  localStorage.setItem('url', __uv$config.prefix + __uv$config.encodeUrl(url));
+  location.href = "/load.html"
+});
